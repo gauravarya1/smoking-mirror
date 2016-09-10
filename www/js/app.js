690c30d62bc1d7652783148046cefadfaa648cfb
gauravarya1 <gauravarya@outlook.com>
2016-09-10 16:08:29 +0530

    Signup Completed added form validation and validation from server for existing user

--- www/js/app.js
+++ www/js/app.js
@@ -33,7 +33,7 @@ var module=angular.module('app', ['ionic', 'app.controllers', 'app.routes', 'app
 
 
 //------------------Controller for Login --------------------------------------------
-module.controller('loginCtrl', function($scope, $http, $ionicPopup, $location) {
+module.controller('loginCtrl', function($scope, $http, $ionicPopup, $location, $state) {
 
     $scope.data = {};
     $scope.users={};
@@ -58,7 +58,25 @@ module.controller('loginCtrl', function($scope, $http, $ionicPopup, $location) {
         data: dataString,
         cache: false,
         success: function(result){
-        alert(result);
+
+        if(result!="fail")
+          {
+            
+            
+                    window.location='/#/home';
+                    //alert("here");
+              
+             
+          }
+        else
+          {
+            var confirmPopup = $ionicPopup.alert
+                        ({
+                            title: 'Login Failed',
+                            template: 'Incorrect username or password'
+                        });
+            
+          }
         }
         });
       }
@@ -162,14 +180,54 @@ module.controller('signupCtrl', function($scope, $http, $ionicPopup, $location) 
  $scope.submitSignup = function(){
    
 
-
- if(( (''+$scope.user.password)!=(''+$scope.user.password2) )||(''+$scope.user.password.length)<2) 
-     {   var confirmPopup = $ionicPopup.alert({
-     title: 'Password Mismatch',
-     template: 'Password and Confirm Password do not match'
-   });
+if(($scope.user.username+"a")=="undefineda")   //Error if username is empty
+    {
+      var confirmPopup = $ionicPopup.alert({
+         title: 'Fill in Username',
+         template: 'Username cannot be empty.'
+       });
     }
-  else if((''+$scope.user.password).length>2) {
+else if(($scope.user.name1+"a")=="undefineda")  //Error if name is empty
+    {
+      var confirmPopup = $ionicPopup.alert({
+         title: 'Fill in Name',
+         template: 'Name cannot be empty.'
+       });
+    }
+else if(($scope.user.password+"a")=="undefineda") //Error if Password is empty
+    {
+      var confirmPopup = $ionicPopup.alert({
+         title: 'Fill in Password',
+         template: 'Password cannot be empty.'
+       });
+    }
+
+else if(($scope.user.email+"a")=="undefineda")     //Error if Email is empty
+    {
+      var confirmPopup = $ionicPopup.alert({
+         title: 'Fill in Email',
+         template: 'Email cannot be empty.'
+       });
+    }
+
+else if(($scope.user.phone+"a")=="undefineda")       //Error if phone number is empty
+    {
+      var confirmPopup = $ionicPopup.alert({
+         title: 'Fill in Phone Number',
+         template: 'Phone Number cannot be empty.'
+       });
+    }
+
+
+ 
+ else if( (''+$scope.user.password)!=(''+$scope.user.password2) )  //Error if Password and Confirm Password do not match.
+         {   var confirmPopup = $ionicPopup.alert({
+         title: 'Password Mismatch',
+         template: 'Password and Confirm Password do not match.'
+       });
+        }
+  else if(((''+$scope.user.password).length>5)&&((''+$scope.user.username).length>4) )
+  {
     
 
 
@@ -186,12 +244,22 @@ module.controller('signupCtrl', function($scope, $http, $ionicPopup, $location) 
                     cache: false,
                  success: function(result)
                  {
-                    var confirmPopup = $ionicPopup.alert
-                    ({
-                        title: 'Hi',
-                        template: result
-                    });
-                    
+                  if(result!="success")
+                  {
+                      var confirmPopup = $ionicPopup.alert
+                      ({
+                          title: result + ' already exists.',
+                          template: 'Please Try a different '+result
+                      });
+                   }
+                   else
+                   {
+                        var confirmPopup = $ionicPopup.alert
+                        ({
+                            title: 'Signup Successful',
+                            template: 'Please login to continue'
+                        });
+                    }
                   }
 
               });
